3dc6fc495085e7db0c0ca7a2ff63bc4eecf2a94c
Vincent Liu <hliu38@hotmail.com>
2018-11-18 17:52:35 -0600

    a lot of changes hf fun making sense of it

--- Application/models.py
+++ Application/models.py
@@ -1,25 +1,23 @@
 from Application import db, login_manager, application
-from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask_login import UserMixin
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 import datetime
 
-@login_manager.user_loader
+
+@login_manager.user_loader # loads in user
 def load_user(user_id):
 	return User.query.get(int(user_id))
 
-# Association Tables
+# Association Tables ("friend's list")
+friends = db.Table('friends', db.Column('self_id', db.Integer, db.ForeignKey('user.id')), db.Column('friend_id', db.Integer, db.ForeignKey('user.id')))
 
-friends = db.Table('friends', 
-	db.Column('self_id', db.Integer, db.ForeignKey('user.id')), 
-	db.Column('friend_id', db.Integer, db.ForeignKey('user.id'))
-	)
-
-
+# Models
 class User(db.Model, UserMixin):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(20), unique=True, nullable=False)
 	email = db.Column(db.String(120), unique=True, nullable=False)
 	password = db.Column(db.String(60), nullable=False)
+	posts = db.relationship('Task', backref='author', lazy=True)
 
 	def get_reset_token(self, expires_sec=1800):
 		s = Serializer(application.config['SECRET_KEY'], expires_sec)
@@ -37,18 +35,19 @@ class User(db.Model, UserMixin):
 	def __repr__(self):
 		return f"User('{self.username}')"
 
-# Models 
-
 class Task(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(20))
-	text = db.Column(db.String(200))
-	complete = db.Column(db.Boolean)
-	#due = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-	duration = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(200))
+	complete = db.Column(db.Boolean, nullable=False, default=True)
+	due = db.Column(db.DateTime, nullable=True)
+	#duration = db.Column(db.Integer, primary_key=True)
+	user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+''' work out events later
 class Event(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(20))
 	text = db.Column(db.String(200))
 	#time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+'''
