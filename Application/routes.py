dba5c8e9c56b9bdbe7943d576175ed9b2620b10c
Vincent Liu <hliu38@hotmail.com>
2018-11-18 19:35:01 -0600

    login fixed

--- Application/routes.py
+++ Application/routes.py
@@ -31,7 +31,7 @@ def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
 		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-		user = User(username=form.username.data, email=form.email.data, password=hashed_password, games_won = 0)
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 		db.session.add(user)
 		db.session.commit()
 		flash('Account creation succesful!', 'success')
