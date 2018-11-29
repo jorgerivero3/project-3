f52be78687646a14a1b8848121a680836e3af13a
Vincent Liu <hliu38@hotmail.com>
2018-11-29 16:05:03 -0600

    changed No due date to Undated and sorted to do list by date

--- Application/routes.py
+++ Application/routes.py
@@ -117,7 +117,7 @@ def reset_token(token):
 def ToDoList():
 	page = request.args.get('page', 1, type=int)
 	tasks = Task.query.filter_by(author=current_user)\
-	.order_by(Task.id.asc())\
+	.order_by(Task.due.asc())\
 	.paginate(page=page, per_page=10)
 	return render_template('/todo_list.html', title='ToDoList', tasks=tasks, user=current_user)
 
